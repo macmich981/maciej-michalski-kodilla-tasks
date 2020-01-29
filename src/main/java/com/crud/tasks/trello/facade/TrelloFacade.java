3f2b1622afe5161f941ec9d1ef1986250742c06d
macmich981 <macmic@onet.eu>
2020-01-30 00:18:00 +0100

    Deleted unused imports from TrelloFacade class

--- src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
+++ src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
@@ -4,8 +4,6 @@ import com.crud.tasks.domain.*;
 import com.crud.tasks.mapper.TrelloMapper;
 import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.validator.TrelloValidator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
