4d3e9d7c86db2a1290ef2ff9f7f90f62896e762e
macmich981 <53463071+macmich981@users.noreply.github.com>
2020-02-19 17:53:28 +0100

    Test.32.1 - arthmetic expression in Thymeleaf (#15)

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -10,6 +10,9 @@ public class StaticWebPageController {
     @RequestMapping("/")
     public String index(Map<String, Object> model) {
         model.put("variable", "My Thymeleaf variable");
+        model.put("product", "2 * 2 = ");
+        model.put("productAndSum", "2 * 2 + 2 = ");
+        model.put("subtractionAndProduct", "2 - 2 * 2 = ");
         model.put("one", 1);
         model.put("two", 2);
         return "index";
