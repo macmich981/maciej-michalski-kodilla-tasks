441de9125915b7b00915d79bc5d12c04d8d147fc
macmich981 <macmic@onet.eu>
2020-02-21 13:05:16 +0100

    Module 32.4 - last refactor for REST API

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -11,18 +11,18 @@ import java.util.List;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("v1/trello")
+@RequestMapping("/v1/trello")
 public class TrelloController {
 
     @Autowired
     private TrelloFacade trelloFacade;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getTrelloBoards")
+    @RequestMapping(method = RequestMethod.GET, value = "/boards")
     public List<TrelloBoardDto> getTrelloBoards() {
         return trelloFacade.fetchTrelloBoards();
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/createTrelloCard")
+    @RequestMapping(method = RequestMethod.POST, value = "/cards")
     public CreatedTrelloCardDto createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
         return trelloFacade.createCard(trelloCardDto);
     }
